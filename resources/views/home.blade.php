accc25c4128aed1232b0940907b5b4310eb3ed31
kingi001 <kingikhamis518@gmail.com>
2024-01-27 21:33:20 +0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,234 @@
+@extends('adminlte::page')
+@section('title', 'Admin Dashboard')
+@section('content_header')
+    <h1>User Management</h1>
+@stop
+@section('content')
+    
+<div style="width:20%">
+<button type="button" class="btn btn-block bg-gradient-primary btn-sm">Add User   <i class='fas fa-user-plus'></i>
+</button>
+</div><br>
+<table  class="table table-striped table-bordered table-sm" style="width:100%">
+  <thead class="bg-light">
+    <tr>
+    <th>#</th>
+    <th>ID</th>
+
+
+      <th>Name</th>
+      <th>Email</th>
+      <th>Contact</th>
+      <th>County</th>
+      <th>Sub-County</th>
+      <th>Area</th>
+      <th>Action</th>
+
+
+
+    </tr>
+  </thead>
+  <tbody>
+
+                            <tr>
+                              <td><input type="checkbox"></td>
+                            <td>1</td>
+
+
+                                <td>Khamis</td>
+                                <td>kingikhamis518@gmail.com</td>
+                                <td>0793532363</td>
+                                <td>mombasa</td>
+                                <td>jomvu</td>
+                                <td>mikindani</td>
+                                <td>
+                                <div class="card-body">
+                                <div class="btn-group"style="margin-top:-25px">
+                                <button  type="button" class="btn btn-warning btn-sm">Action</button>
+                                <button type="button" class="btn btn-warning dropdown-toggle dropdown-icon btn-sm" data-toggle="dropdown">
+                                <span class="sr-only">Toggle Dropdown</span>
+                                </button>
+                                <div class="dropdown-menu" role="menu">
+                                <a class="dropdown-item" href="#"><i class="fas fa-edit"></i>Update</a>
+
+                                <a class="dropdown-item" href="#"><i class="fas fa-power-off"></i>Deactivate</a>
+
+                                </div>
+                            </div>
+                            </div>
+             
+                                </td>
+                            </tr>
+                            <tr>
+                            <td><input type="checkbox"></td>
+                            <td>2</td>
+                                <td>Khamis</td>
+                                <td>kingikhamis518@gmail.com</td>
+                                <td>0793532363</td>
+                                <td>mombasa</td>
+                                <td>jomvu</td>
+                                <td>mikindani</td>
+                                <td>
+                                <div class="card-body">
+                                <div class="btn-group"style="margin-top:-25px">
+                                <button  type="button" class="btn btn-warning btn-sm">Action</button>
+                                <button type="button" class="btn btn-warning dropdown-toggle dropdown-icon btn-sm" data-toggle="dropdown">
+                                <span class="sr-only">Toggle Dropdown</span>
+                                </button>
+                                <div class="dropdown-menu" role="menu">
+                                <a class="dropdown-item" href="#"><i class="fas fa-edit"></i>Update</a>
+
+                                <a class="dropdown-item" href="#"><i class="fas fa-power-off"></i>Deactivate</a>
+
+                                </div>
+                            </div>
+                            </div>
+             
+                                </td>
+                            </tr>
+                            <tr>
+                            <td><input type="checkbox"></td>
+                            <td>3</td>
+                                <td>Khamis</td>
+                                <td>kingikhamis518@gmail.com</td>
+                                <td>0793532363</td>
+                                <td>mombasa</td>
+                                <td>jomvu</td>
+                                <td>mikindani</td>
+                                <td>
+                                <div class="card-body">
+                                <div class="btn-group"style="margin-top:-25px">
+                                <button  type="button" class="btn btn-warning btn-sm">Action</button>
+                                <button type="button" class="btn btn-warning dropdown-toggle dropdown-icon btn-sm" data-toggle="dropdown">
+                                <span class="sr-only">Toggle Dropdown</span>
+                                </button>
+                                <div class="dropdown-menu" role="menu">
+                                <a class="dropdown-item" href="#"><i class="fas fa-edit"></i>Update</a>
+
+                                <a class="dropdown-item" href="#"><i class="fas fa-power-off"></i>Deactivate</a>
+
+                                </div>
+                            </div>
+                            </div>
+             
+                                </td>
+                            </tr>
+                            <tr>
+                              <td><input type="checkbox"></td>
+                            <td>1</td>
+
+
+                                <td>Khamis</td>
+                                <td>kingikhamis518@gmail.com</td>
+                                <td>0793532363</td>
+                                <td>mombasa</td>
+                                <td>jomvu</td>
+                                <td>mikindani</td>
+                                <td>
+                                <div class="card-body">
+                                <div class="btn-group"style="margin-top:-25px">
+                                <button  type="button" class="btn btn-warning btn-sm">Action</button>
+                                <button type="button" class="btn btn-warning dropdown-toggle dropdown-icon btn-sm" data-toggle="dropdown">
+                                <span class="sr-only">Toggle Dropdown</span>
+                                </button>
+                                <div class="dropdown-menu" role="menu">
+                                <a class="dropdown-item" href="#"><i class="fas fa-edit"></i>Update</a>
+
+                                <a class="dropdown-item" href="#"><i class="fas fa-power-off"></i>Deactivate</a>
+
+                                </div>
+                            </div>
+                            </div>
+             
+                                </td>
+                            </tr>
+                            <tr>
+                            <td><input type="checkbox"></td>
+                            <td>2</td>
+                                <td>Khamis</td>
+                                <td>kingikhamis518@gmail.com</td>
+                                <td>0793532363</td>
+                                <td>mombasa</td>
+                                <td>jomvu</td>
+                                <td>mikindani</td>
+                                <td>
+                                <div class="card-body">
+                                <div class="btn-group"style="margin-top:-25px">
+                                <button  type="button" class="btn btn-warning btn-sm">Action</button>
+                                <button type="button" class="btn btn-warning dropdown-toggle dropdown-icon btn-sm" data-toggle="dropdown">
+                                <span class="sr-only">Toggle Dropdown</span>
+                                </button>
+                                <div class="dropdown-menu" role="menu">
+                                <a class="dropdown-item" href="#"><i class="fas fa-edit"></i>Update</a>
+
+                                <a class="dropdown-item" href="#"><i class="fas fa-power-off"></i>Deactivate</a>
+
+                                </div>
+                            </div>
+                            </div>
+             
+                                </td>
+                            </tr>
+                            <tr>
+                            <td><input type="checkbox"></td>
+                            <td>3</td>
+                                <td>Khamis</td>
+                                <td>kingikhamis518@gmail.com</td>
+                                <td>0793532363</td>
+                                <td>mombasa</td>
+                                <td>jomvu</td>
+                                <td>mikindani</td>
+                                <td>
+                                <div class="card-body">
+                                <div class="btn-group"style="margin-top:-25px">
+                                <button  type="button" class="btn btn-warning btn-sm">Action</button>
+                                <button type="button" class="btn btn-warning dropdown-toggle dropdown-icon btn-sm" data-toggle="dropdown">
+                                <span class="sr-only">Toggle Dropdown</span>
+                                </button>
+                                <div class="dropdown-menu" role="menu">
+                                <a class="dropdown-item" href="#"><i class="fas fa-edit"></i>Update</a>
+
+                                <a class="dropdown-item" href="#"><i class="fas fa-power-off"></i>Deactivate</a>
+
+                                </div>
+                            </div>
+                            </div>
+             
+                                </td>
+                            </tr>
+                            <tr>
+                            <td><input type="checkbox"></td>
+                            <td>3</td>
+                                <td>Khamis</td>
+                                <td>kingikhamis518@gmail.com</td>
+                                <td>0793532363</td>
+                                <td>mombasa</td>
+                                <td>jomvu</td>
+                                <td>mikindani</td>
+                                <td>
+                                <div class="card-body">
+                                <div class="btn-group"style="margin-top:-25px">
+                                <button  type="button" class="btn btn-warning btn-sm">Action</button>
+                                <button type="button" class="btn btn-warning dropdown-toggle dropdown-icon btn-sm" data-toggle="dropdown">
+                                <span class="sr-only">Toggle Dropdown</span>
+                                </button>
+                                <div class="dropdown-menu" role="menu">
+                                <a class="dropdown-item" href="#"><i class="fas fa-edit"></i>Update</a>
+
+                                <a class="dropdown-item" href="#"><i class="fas fa-power-off"></i>Deactivate</a>
+
+                                </div>
+                            </div>
+                            </div>
+             
+                                </td>
+                            </tr>
+                     
+                     
+
+    
+  </tbody>
+</table>
+
+@stop
+
